dc199196efde8f888af4e29efa70e21c5e8ba948
JersonNetcol <j.espitia@netcol.net.co>
2026-01-21 10:29:01 -0500

    ii

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://iskra.com.co', // tu dominio
+  generateRobotsTxt: true,         // crea también robots.txt
+  sitemapSize: 5000,                // opcional, útil si muchas páginas
+}
